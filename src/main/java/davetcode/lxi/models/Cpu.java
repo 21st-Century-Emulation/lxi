e2c7fca911cd9c3a4ba74ff57c49cd636ccb7e55
David Tyler <david.tyler@metaswitch.com>
2021-04-24 22:37:02 +0100

    Added cpu id

--- src/main/java/davetcode/lxi/models/Cpu.java
+++ src/main/java/davetcode/lxi/models/Cpu.java
@@ -3,10 +3,13 @@ package davetcode.lxi.models;
 public class Cpu {
   private short Opcode;
 
+  private String Id;
+
   private CpuState State;
 
-  public Cpu(short opcode, CpuState state) {
+  public Cpu(short opcode, String id, CpuState state) {
     setOpcode(opcode);
+    setId(id);
     setState(state);
   }
 
@@ -16,6 +19,14 @@ public class Cpu {
 
   public void setState(CpuState state) {
     this.State = state;
+  }
+
+  public String getId() {
+    return Id;
+  }
+
+  public void setId(String id) {
+    this.Id = id;
   }
 
   public short getOpcode() {
--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ sleep 5
 
 RESULT=$(curl -s --header "Content-Type: application/json" \
   --request POST \
-  --data '{"opcode":1,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":0,"l":0,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":true},"programCounter":0,"stackPointer":0,"cycles":0}}' \
+  --data '{"id":"abcd", "opcode":1,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":0,"l":0,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":true},"programCounter":0,"stackPointer":0,"cycles":0}}' \
   http://localhost:8080/api/v1/execute\?operand2=1\&operand1=2)
-EXPECTED='{"opcode":1,"state":{"a":181,"b":1,"c":2,"d":0,"e":0,"h":0,"l":0,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":true},"programCounter":0,"stackPointer":0,"cycles":10}}'
+EXPECTED='{"id":"abcd", "opcode":1,"state":{"a":181,"b":1,"c":2,"d":0,"e":0,"h":0,"l":0,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":true},"programCounter":0,"stackPointer":0,"cycles":10}}'
 
 docker kill lxi
 
